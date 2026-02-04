32db24f089255f4947eeee48587c27e03c105431
Guillem <guillem@disedit.com>
2026-02-04 17:23:20 +0100

    Update HomeHero.vue

--- storyblok/HomeHero.vue
+++ storyblok/HomeHero.vue
@@ -15,7 +15,7 @@ const backgroundStyle = computed(() => {
 })
 
 const { y } = useWindowScroll()
-const scrolled = computed(() => y.value > 800)
+const scrolled = computed(() => y.value > 700)
 </script>
 
 <template>
